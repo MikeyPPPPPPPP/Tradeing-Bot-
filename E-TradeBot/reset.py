b123cd02206ed2eb44720af8c112b93c6a47ac4e
Michael Provenzano <50926811+MikeyPPPPPPPP@users.noreply.github.com>
2022-01-29 21:02:27 -0800

    Update reset.py

--- E-TradeBot/reset.py
+++ E-TradeBot/reset.py
@@ -1,6 +1,7 @@
 import subprocess
 import time
 
+'''disregard this, turns out I was pressing control x instead of control c'''
 
 def reset():
     '''this will kill the app.py program so we don need to do it manually every time'''
